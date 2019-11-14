c5b3210a204bba94ae1fef340e769ac942194c53
Knight <janae.jpeg@gmail.com>
2019-11-14 10:31:13 -0600

    gen update

---
+++ java/javascript.js
@@ -0,0 +1,11 @@
+function getRes() {
+
+    let toGet = document.getElementById("toGet").value;
+    let curRes = document.getElementById("curRes"); 
+
+
+
+    curRes.innerHTML = " =";
+
+    console.log(toGet);
+}
